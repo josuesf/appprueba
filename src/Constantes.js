5803af08986b722d1ce4536b2870fe1de15f605f
josuesf <113552@unsaac.edu.pe>
2018-05-25 14:36:28 -0500

    version 1.0.0.3.2

--- src/Constantes.js
+++ src/Constantes.js
@@ -1,5 +1,5 @@
-export const URL_WS='http://192.168.1.6:5000'
+// export const URL_WS='http://192.168.1.6:5000'
 // export const URL_WS='http://192.168.1.249:5000'
-//export const URL_WS='http://192.168.0.12:5000'
+export const URL_WS='http://192.168.1.254:5000'
 
 // export const URL_WS='http://misky.hopto.org:5000'
